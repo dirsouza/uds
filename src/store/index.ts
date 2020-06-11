6699985749a7c9c3387487f51c94a39ae66806e7
Diogo Souza <diogo.souza@msn.com>
2020-06-11 19:32:16 -0400

    adicionado criptografia e o módulo de pedido

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import SecureLs from 'secure-ls'
+import Pedido from "@/store/modules/pedido";
 
 Vue.use(Vuex)
+
+const ls = new SecureLs({
+  isCompression: true,
+  encodingType: 'aes',
+  encryptionSecret: process.env.VUE_APP_VUEX_SECRET
+})
 
 export default new Vuex.Store({
   state: {
@@ -11,5 +20,15 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+    Pedido
+  },
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key)
+      }
+    })
+  ]
 })
